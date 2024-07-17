5d6e99e3bf0abebcfa0dad457821238d0fbcd9cd
skid-kid <polarb587@gmail.com>
2024-07-17 23:26:29 +0530

    little changes

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import farmer from './assets/farmer.jpg'
+import farmer from './assets/farmer.png'
 import farmer2 from './assets/farmer2.png'
 import { Form } from './components/form1'
 const text="{AI}"
@@ -20,7 +20,10 @@ function App() {
   </h2>
 </div>
    <div className="container flex justify-between m-0 p-0 "> 
-   <img className="brightness-120" src={farmer2} style={{width:"700px"}}></img> 
+    <div className=''>
+   <img className="brightness-120" src={farmer2} style={{width:"1200px"}}></img>
+   <img className='' src={farmer}></img>
+   </div> 
    <div className=" m-0 p-0 drop-shadow-xl " style={{height:"200px"}}>
    <h2 className="font-sans justify-center text-center m-10 p-15  text-pretty font-bold text-purple-400 drop-shadow-xl" style={{fontSize:"40px"}}>GET HELP IN CHECKING <a className='text-black'>{Soil}</a> BY FILLING THIS FORM AFTER GETTING THE VALUE OF PARTICULAR FIELDS</h2>
    <h4 className='font-sans justfiy-center text-center '>*Provide float values only*</h4>
